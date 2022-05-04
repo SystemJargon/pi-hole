b6f1d86da7367b18253fd90e86b673ff9770ad49
L <24641464+lz-eng@users.noreply.github.com>
2022-05-05 07:52:59 +1200

    Update get-lists.sh
    simplify lists to three lists only

--- scripts/get-lists.sh
+++ scripts/get-lists.sh
@@ -5,13 +5,9 @@
 
 # this will PUT ALL lists below into the "default" group.
 
-sudo sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (address, enabled, comment) VALUES (' https://raw.githubusercontent.com/chadmayfield/my-pihole-blocklists/master/lists/pi_blocklist_porn_all.list', 1, 'Porn');"
-sudo sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (address, enabled, comment) VALUES ('https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/SmartTV.txt', 1, 'SmartTV');"
+sudo sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (address, enabled, comment) VALUES ('https://github.com/lz-eng/pihole/raw/main/dynamic-big-list/firebog-ticklist.list', 1, 'Porn');"
+sudo sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (address, enabled, comment) VALUES ('https://github.com/lz-eng/pihole/raw/main/dynamic-big-list/pihole-blocklist-phish-malware-nsfw.txt', 1, 'SmartTV');"
 sudo sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (address, enabled, comment) VALUES ('https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts', 1, 'StevenBlack_Hosts');"
-sudo sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (address, enabled, comment) VALUES ('https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt', 1, 'Windows-SpyBlocker');"
-sudo sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (address, enabled, comment) VALUES ('https://raw.githubusercontent.com/elliotwutingfeng/Inversion-DNSBL-Blocklists/main/Google_hostnames.txt', 1, 'DNSBL-Blocklists');"
-sudo sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (address, enabled, comment) VALUES ('https://raw.githubusercontent.com/elliotwutingfeng/GlobalAntiScamOrg-blocklist/main/global-anti-scam-org-scam-urls.txt', 1, 'Global AntiScam URLs');"
-sudo sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (address, enabled, comment) VALUES ('https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt', 1, 'AdServers');"
 
 
 
@@ -26,15 +22,8 @@ pihole -g
 # uncomment lines below if you wish to download lists and maintain your own versioning and updates
 # cd ~
 # mkdir pihole-lists && cd pihole-lists
-# wget https://raw.githubusercontent.com/elliotwutingfeng/Inversion-DNSBL-Blocklists/main/Google_hostnames.txt
-# wget https://raw.githubusercontent.com/elliotwutingfeng/GlobalAntiScamOrg-blocklist/main/global-anti-scam-org-scam-urls.txt
-# wget https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt
-# wget https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/SmartTV.txt
-# wget https://adaway.org/hosts.txt
-# wget https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt
-# wget https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt
-# wget https://raw.githubusercontent.com/chadmayfield/my-pihole-blocklists/master/lists/pi_blocklist_porn_all.list
+# wget https://github.com/lz-eng/pihole/raw/main/dynamic-big-list/firebog-ticklist.list
+# wget https://github.com/lz-eng/pihole/raw/main/dynamic-big-list/pihole-blocklist-phish-malware-nsfw.txt
 # wget https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
-
 
 # EOF
