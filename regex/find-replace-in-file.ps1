ac6cf8a341c379a46c069f878cd8cd719bd6f2b9
L <24641464+lz-eng@users.noreply.github.com>
2022-05-05 14:50:38 +1200

    Delete find-replace-in-file.ps1

--- regex/find-replace-in-file.ps1
+++
@@ -1,9 +0,0 @@
-$filePath = "$env:USERPROFILE\Downloads\input.txt"
-$tempFilePath = "$env:TEMP\$($filePath | Split-Path -Leaf)"
-$find = 'badword'
-$replace = 'goodword'
-
-(Get-Content -Path $filePath) -replace $find, $replace | Add-Content -Path $tempFilePath
-
-Remove-Item -Path $filePath
-Move-Item -Path $tempFilePath -Destination $filePath
